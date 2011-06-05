f788fd4f4b0bbe91e6c9cf0796e87d860fda90cc
Mahipal Raythattha <mahipalr@gmail.com>
2011-06-04 18:10:12 -0700

    Initial commit. New project from Window template with frameworks added.

---
+++ Classes/PlausibleBooksAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PlausibleBooksAppDelegate.h
+//  PlausibleBooks
+//
+//  Created by Mahipal Raythattha on 6/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlausibleBooksAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
